d7868c182d84f00493f6b6266cdd10b614ee9e1b
amo3223 <50189394+amo3223@users.noreply.github.com>
2020-06-04 20:49:23 +0800

    Update processorder.php

--- processorder.php
+++ processorder.php
@@ -1,3 +1,11 @@
 <?php
-echo "Hello world";
+function randomkeys($length){   
+   $output='';   
+   for ($a = 0; $a<$length; $a++) {   
+       $output .= chr(mt_rand(65, 90));    //生成php隨機數   
+    }   
+    return $output;   
+}   
+echo randomkeys(14); 
+
 ?>
