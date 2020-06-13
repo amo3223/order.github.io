214dc2710ce9240e5e5cbe45204af646d38bdfdb
amo3223 <50189394+amo3223@users.noreply.github.com>
2020-06-13 14:09:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-function randomWord(randomFlag, min, max){
-	var str = "",
-		range = min,
-		arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
-	// 随机产生
-	if(randomFlag){
-		range = Math.round(Math.random() * (max-min)) + min;
-	}
-	for(var i=0; i<range; i++){
-		pos = Math.round(Math.random() * (arr.length-1));
-		str += arr[pos];
-	}
-	return str;
+<script> 
+function GetRandomNum(Min,Max)
+{ 
+var Range = Max - Min; 
+var Rand = Math.random(); 
+return(Min   Math.round(Rand * Range)); 
+} 
+var num = GetRandomNum(1,10); 
+alert(num); 
+</script>
+var chars = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+function generateMixed(n) {
+var res = "";
+for(var i = 0; i < n ; i   ) {
+var id = Math.ceil(Math.random()*35);
+res  = chars[id];
 }
-
-
-// 方案二
-// 指定具体位数
-
-function getRandomStr(len) {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for( var i=0; i < len; i++ )
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    return text;
+return res;
 }
