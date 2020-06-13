e1842b07014147d20fb3f04a038c8fa78f3ab793
amo3223 <50189394+amo3223@users.noreply.github.com>
2020-06-13 14:05:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-<script language=”javascript”>
-function randomString(len) {
-　　len = len || 32;
-　　var $chars = ‘ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678’;    /****預設去掉了容易混淆的字元oOLl,9gq,Vv,Uu,I1****/
-　　var maxPos = $chars.length;
-　　var pwd = ”;
-　　for (i = 0; i < len; i ) {
-　　　　pwd = $chars.charAt(Math.floor(Math.random() * maxPos));
-　　}
-　　return pwd;
+function randomWord(randomFlag, min, max){
+	var str = "",
+		range = min,
+		arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+	// 随机产生
+	if(randomFlag){
+		range = Math.round(Math.random() * (max-min)) + min;
+	}
+	for(var i=0; i<range; i++){
+		pos = Math.round(Math.random() * (arr.length-1));
+		str += arr[pos];
+	}
+	return str;
 }
-document.write(randomString(32));
-</script>
+
+
+// 方案二
+// 指定具体位数
+
+function getRandomStr(len) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for( var i=0; i < len; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
