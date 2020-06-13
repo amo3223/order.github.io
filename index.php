43fd00147c351ee69e24283cad9617a8c32f1fa2
amo3223 <50189394+amo3223@users.noreply.github.com>
2020-06-13 11:13:24 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
     $title = '訂單成立';
     $string = '已收到訂單';
+    $stringa = '萬分感謝你的惠顧';
+    $stringb = '本店提供台灣本島寄送服務(不接受郵政信箱地址),暫無提供離島,確認付款完成 我們會在30分鐘到一小時內將商品寄到你的收貨地址。';
+            
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -41,7 +44,7 @@
              }
  
  
-             .red-box {
+             .black-box {
              background-color: 	#fa8072;
              color: black;
              padding: 20px 40px 20px 40px;
@@ -60,6 +63,40 @@
     <div class="plan">
         <?php echo $string; ?>
     </div>
-   
+    <?php
+function getrand_id(){
+    $id_len = 10;//字串長度
+    $id = '';
+    $word = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';//字典檔 你可以將 數字 0 1 及字母 O L 排除
+    $len = strlen($word);//取得字典檔長度
+ 
+    for($i = 0; $i < $id_len; $i++){ //總共取 幾次
+        $id .= $word[rand() % $len];//隨機取得一個字元
+    }
+    return $id;//回傳亂數帳號
+}
+ 
+$a=array();//初始化一個陣列要來存放所產生的亂數
+ 
+for($x=0;$x<10;$x++){ //$x=>要取得幾筆亂數帳號
+    $b=getrand_id();//取得亂數帳號
+    if(!in_array($b,$a)){//判斷有沒有重覆
+        array_push($a,$b);//將產生的亂數帳號加入陣列
+    }else{$x-=1;}//有重覆再重新產生一筆
+}
+ 
+for($x=0;$x<1;$x++){
+    echo $a[$x];//顯示結果
+}
+?>
+    <div class="red-text">
+        <div class="box black-box">
+        <?php 
+            echo $stringa; 
+            echo $stringb;
+        ?>
+        </div>    
+    </div>
+        
 </body>
 </html>
